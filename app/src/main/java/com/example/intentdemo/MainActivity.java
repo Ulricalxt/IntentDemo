dd6e5a51da8334c838a3e35096bfefcbacc23f39
SP6 <qwer14788lll@163.com>
2021-05-26 09:19:04 +0800

    项目初始化

---
+++ app/src/main/java/com/example/intentdemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.intentdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author 龚鸿飞
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
