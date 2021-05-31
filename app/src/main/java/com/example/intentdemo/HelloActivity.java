a506f6d16b8efd92426973ea85dba43c1eaf1a65
SP6 <qwer14788lll@163.com>
2021-05-31 14:30:14 +0800

    创建了HelloActivity，用于显示跳转演示

---
+++ app/src/main/java/com/example/intentdemo/HelloActivity.java
@@ -0,0 +1,17 @@
+package com.example.intentdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author Surface Pro 6
+ */
+public class HelloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+    }
+}
