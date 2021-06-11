ef1783cf533eea99370588160c918f4b469a35da
李鑫熠 <2466758288@qq.com>
2021-06-11 09:39:30 +0800

    该走了activit_hi,使其响应com.my.test的动作

---
+++ app/src/main/java/com/example/intentdemo/Activity_hi.java
@@ -0,0 +1,14 @@
+package com.example.intentdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_hi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hi);
+    }
+}
